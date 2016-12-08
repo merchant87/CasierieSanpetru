586ce3c43af345c6e332ec1d8bc3d7fb5dc57258
Liviu Sabau <liviusabau@gmail.com>
2016-12-08 22:08:00 +0200

    Removed commented code in main class

--- src/casieriesanpetruexcel/CasierieSanpetruExcel.java
+++ src/casieriesanpetruexcel/CasierieSanpetruExcel.java
@@ -25,33 +25,6 @@ public class CasierieSanpetruExcel {
     
     public CasierieSanpetruExcel(){
         mainGUI = new MainGUI();
-        /*
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(SwingGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(SwingGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(SwingGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(SwingGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new SwingGUI().setVisible(true);
-            }
-        });
-        */
     }
-    
-    
-    
-    
+
 }
